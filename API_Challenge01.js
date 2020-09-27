48b4f8eab4e7046ba68323477902009bf10fd54b
IndyJoe <IndyJoeBRJS@gmail.com>
2020-09-27 14:24:05 -0400

    udpate to final push

--- API_Challenge01.js
+++ API_Challenge01.js
@@ -1,3 +1,4 @@
+//   *****   DECLARATIONS   *****
 const catFactsURL = "https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=";
 let queryURL;     // declared only; instialized after fetch
 
@@ -13,8 +14,10 @@ const displaySection = document.querySelector('section');
 let theCatFactToDisplay = "";
 
 // Number of results
-let qtyFacts = 1;
-let maxFactsAllowed = 5;
+let qtyFacts = 1;           // The initial number of facts
+let maxFactsAllowed = 5;    // Maximum facts allowed
+    // The site allows up to 500, but more than 5 facts makes the chances
+    // likely that the "tongue" will extend off the bottom of the webpage.
 
 // Event Listeners
 btnMore.addEventListener('click', moreFacts);   // listening +facts btn
@@ -22,7 +25,7 @@ btnLess.addEventListener('click', fewerFacts);  // listening -facts btn
 btnGetFacts.addEventListener('click', getFacts);// listening get facts btn
 
 
-//  ***  FETCH RESULTS  ***
+//   *****   FETCH RESULTS   *****
 function fetchResults(queryURL) {  // fetch data from URL
   fetch(queryURL)
     .then(function(response) {     // gets results, pass into 'response'
@@ -34,11 +37,7 @@ function fetchResults(queryURL) {  // fetch data from URL
 };
 
 
-
-
-
-
-//  ***  DISPLAY RESULTS  ***
+//   *****   DISPLAY RESULTS   *****
 function displayResultsLoop(json) {
 
   // Loop removes previous facts
@@ -46,82 +45,66 @@ function displayResultsLoop(json) {
     displaySection.removeChild(displaySection.firstChild);  
   } 
 
-  // IF determines if incoming data is array or object
+  // IF determines wether incoming data will be an array(1) or object(2+)
   if(qtyFacts === 1) {    // API sends one array
-    //console.log(json.text)           // *****  DELETE THIS   *****
     displayResultsToPage(json.text);
   } else {
     for(let i = 0; i < qtyFacts; i++) {   // API send arrays in one object
-      //console.log(json[i].text);           // *****  DELETE THIS   *****
       displayResultsToPage(json[i].text);
     }
   }   // End of iteration display loop
 
 
   function displayResultsToPage(theCatFactToDisplay) {
-    console.log(theCatFactToDisplay);           // *****  DELETE THIS   *****
 
+    // Creates <h6> for fact and puts fact into it
     let displayText = document.createElement('h6');
     displayText.textContent = theCatFactToDisplay;
  
+    // Creates <div> for <h6> and gives it the aCatFact class (for CSS)
     let displayDiv = document.createElement('div');
     displayDiv.className = 'aCatFact';
 
+    // Puts cat fact <h> in the <div> and puts <div> in the <section>
     displayDiv.appendChild(displayText);
     displaySection.appendChild(displayDiv);
-
-
   }   //  End of displayResultsToPage Function
-
-
-
 
 };  //  END OF displayResultsLoop Function
 
 
-
-
-
-//  *****   BUTTON FUNCTIONS   *****
-
+//   *****   BUTTON FUNCTIONS   *****
 //  Get Facts button
 function getFacts(e) {
-  console.log("--------------------------");
-  console.log("What is passed in 'e':");
-  console.log(e);
-  console.log("--------------------------");
   updateDisplay(qtyFacts)
 };    // END OF getFacts Function
-
 
 // One More Button
 function moreFacts(e) {
   if(qtyFacts < maxFactsAllowed) {
     qtyFacts++;
   } else {
-    window.alert(`You may only receive a maximum of ${maxFactsAllowed} cat facts at a time!`);
+    window.alert(`You may only receive a maximum of ${maxFactsAllowed} cat facts at a time!`);    // alert explains max cat facts allowed
     return;
   }
-  showQtyFacts.innerText = qtyFacts;
+  showQtyFacts.innerText = qtyFacts;   // updates # of facts chosen
 };      //  END OF moreFacts Function
-
 
 // One Less Button
 function fewerFacts(e) {   
   if(qtyFacts > 1) {
     qtyFacts--;
   } else {
-    window.alert(`Certainly you want at least one cat fact!`);
+    window.alert(`Certainly you want at least one cat fact!`);  // alert explains min cat facts allowed
     return;
   }
-  showQtyFacts.innerText = qtyFacts;
+  showQtyFacts.innerText = qtyFacts;   // updates # of facts chosen
 };      //  END OF fewerFacts Function
 
 
-// Update Display (the primary execution function)
+//   *****   PRIMARY EXECUTION FUNCTION   *****
+// Update Display
 function updateDisplay(qtyFacts) {
-  console.log("# of Facts:", qtyFacts);           // *****  DELETE THIS   *****
-  queryURL = catFactsURL + qtyFacts;
-  console.log("The URL:", queryURL);              // *****  DELETE THIS   *****
-  fetchResults(queryURL);
+  queryURL = catFactsURL + qtyFacts;     // creates URL for fetch
+  fetchResults(queryURL);                // call fetch with new URL
 };    // END OF UPDATE DISPLAY
